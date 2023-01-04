aff031962b95f8c1d586b5391e3395c062709e7d
crinai <crinai@crinai.com>
2023-01-04 21:11:37 +0800

    Update Pay.php

--- src/Pay.php
+++ src/Pay.php
@@ -8,7 +8,7 @@
  * This source file is subject to the MIT license that is bundled.
  */
 
-namespace Guanguans\YiiPay;
+namespace crinai\YiiPay;
 
 use Yansongda\Pay\Log;
 use Yansongda\Pay\Pay as YsdPay;
